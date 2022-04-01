1b6a2627f42325abf92344bb2212d93699b58120
Stefan <stefan.kewatt@gmail.com>
2022-04-01 00:07:01 -0500

    Add tests for Signature

--- src/types/signature.rs
+++ src/types/signature.rs
@@ -76,7 +76,7 @@ impl From<&[u8]> for Signature {
         let len = value.len();
         let mut result: [u8; 4] = [0x20; 4];
         match len {
-            i if i <= 0 => result = [0, 0, 0, 0],
+            i if i <= 0 => result = result,
             i if i == 1 => result[..1].copy_from_slice(&value[..1]),
             i if i == 2 => result[..2].copy_from_slice(&value[..2]),
             i if i == 3 => result[..3].copy_from_slice(&value[..3]),
@@ -96,5 +96,156 @@ impl From<Signature> for String {
 impl From<&str> for Signature {
     fn from(s: &str) -> Self {
         Self::from(s.as_bytes())
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::types::signatures::color_space;
+
+    use super::*;
+
+    #[test]
+    fn test_signature_gray_is_correct() {
+        let expected = u32::from_be_bytes(*b"GRAY");
+        let actual = Signature::new(b"GRAY").0;
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_signature_from_str_is_correct() {
+        let expected = color_space::GRAY;
+        let actual = Signature::from("GRAY");
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_signature_default_is_4_spaces() {
+        let expected = Signature::new(b"    ");
+        let actual: Signature = Default::default();
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_signature_from_array_of_4_is_correct() {
+        let expected = color_space::GRAY;
+        let actual = Signature::from(b"GRAY");
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_signature_from_array_of_3_is_correct() {
+        let expected = color_space::RGB;
+        let actual = Signature::from(b"RGB");
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_signature_from_array_of_2_is_correct() {
+        let expected = Signature::new(b"xy  ");
+        let actual = Signature::from(b"xy");
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_signature_from_array_of_1_is_correct() {
+        let expected = Signature::new(b"a   ");
+        let actual = Signature::from(b"a");
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_signature_from_slice_of_more_than_4_is_correct() {
+        let expected = Signature::new(b"abcd");
+        let actual = Signature::from(b"abcdefg".as_slice());
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_signature_from_slice_of_4_is_correct() {
+        let expected = color_space::GRAY;
+        let actual = Signature::from(b"GRAY".as_slice());
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_signature_from_slice_of_3_is_correct() {
+        let expected = color_space::RGB;
+        let actual = Signature::from(b"RGB".as_slice());
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_signature_from_slice_of_2_is_correct() {
+        let expected = Signature::new(b"xy  ");
+        let actual = Signature::from(b"xy".as_slice());
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_signature_from_slice_of_1_is_correct() {
+        let expected = Signature::new(b"a   ");
+        let actual = Signature::from(b"a".as_slice());
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_signature_from_slice_of_0_is_default() {
+        let expected: Signature = Default::default();
+        let actual = Signature::from(&b"a"[0..0]);
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_signature_from_u32_is_correct() {
+        let expected = color_space::GRAY;
+        let actual = Signature::from(u32::from_be_bytes(*b"GRAY"));
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_signature_to_u32_is_correct() {
+        let expected = u32::from_be_bytes(*b"GRAY");
+        let actual: u32 = color_space::GRAY.into();
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_signature_to_array_of_4_is_correct() {
+        let expected = *b"GRAY";
+        let actual: [u8; 4] = color_space::GRAY.into();
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_signature_to_string_is_correct() {
+        let expected = "GRAY".to_string();
+        let actual: String = color_space::GRAY.into();
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_signature_to_string_with_invalid_data_is_default() {
+        let expected: String = Signature::default().into();
+        let actual: String = Signature::new(&[0, 159, 146, 150]).into();
+
+        assert_eq!(expected, actual);
     }
 }
