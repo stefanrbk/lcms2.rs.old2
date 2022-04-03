be7a55d5f9a9abbddaeb94124178500fdf0ce296
Stefan Kewatt <stefan.kewatt@gmail.com>
2022-04-03 17:22:45 -0500

    Add Profile and a few methods

--- src/types/mod.rs
+++ src/types/mod.rs
@@ -1,13 +1,17 @@
 mod date_time_number;
 mod encoded_xyz_number;
 mod icc_header;
+mod profile;
 mod profile_id;
 mod signature;
 
 pub use date_time_number::DateTimeNumber;
 pub use encoded_xyz_number::EncodedXYZNumber;
 pub use icc_header::ICCHeader;
+pub use profile::Profile;
 pub use profile_id::ProfileID;
 pub use signature::Signature;
 
 pub mod signatures;
+
+pub const MAX_TABLE_TAG: usize = 100;
---
+++ src/types/profile.rs
@@ -0,0 +1,83 @@
+use std::{fs::{OpenOptions, File}, io::{Read, self}, io::Write};
+
+use chrono::Utc;
+
+use crate::state::Context;
+
+use super::{Signature, ProfileID, MAX_TABLE_TAG};
+
+pub struct Profile<'a> {
+    context: Option<Context>,
+    reader: Option<&'a dyn Read>,
+    writer: Option<&'a dyn Write>,
+    created: chrono::NaiveDateTime,
+    version: u32,
+    device_class: Signature,
+    color_space: Signature,
+    pcs: Signature,
+    rendering_intent: u32,
+    flags: u32,
+    manufacturer: u32,
+    model: u32,
+    attributes: u64,
+    creator: u32,
+    profile_id: ProfileID,
+    tag_count: u32,
+    tag_names: [Signature; MAX_TABLE_TAG],
+    tag_linked: [Option<Signature>; MAX_TABLE_TAG],
+    tag_sizes: [usize; MAX_TABLE_TAG],
+    tag_offsets: [usize; MAX_TABLE_TAG],
+    tag_save_as_raw: [bool; MAX_TABLE_TAG],
+    is_write: bool
+}
+
+impl Profile<'_> {
+    pub fn create_placeholder(context: Context) -> Box<Self> {
+        Box::new(Self {
+            context: Some(context),
+            tag_count: 0,
+            version: 0x02100000,
+            created: Utc::now().naive_utc(),
+            // mutex stuff for the future goes here
+            device_class: Signature::default(),
+            color_space: Signature::default(),
+            pcs: Signature::default(),
+            rendering_intent: 0,
+            flags: 0,
+            manufacturer: 0,
+            model: 0,
+            attributes: 0,
+            creator: 0,
+            profile_id: ProfileID { id16: [0u16; 8] },
+            tag_names: [Signature::default(); MAX_TABLE_TAG],
+            tag_linked: [None; MAX_TABLE_TAG],
+            tag_sizes: [0; MAX_TABLE_TAG],
+            tag_offsets: [0; MAX_TABLE_TAG],
+            tag_save_as_raw: [false; MAX_TABLE_TAG],
+            is_write: false,
+            reader: None,
+            writer: None,
+        })
+    }
+
+    pub fn open_from_file(context: Context, filename: String, mode: FileMode) -> io::Result<()> {
+        let mut profile = Self::create_placeholder(context);
+
+        match mode {
+            Read => {
+                profile.reader = Some(&File::open(filename)?);
+            },
+            Write => {
+                profile.writer = Some(&File::create(filename)?);
+                profile.is_write = true;
+            }
+        };
+
+        Ok(())
+    }
+}
+
+pub enum FileMode {
+    Read,
+    Write
+}
