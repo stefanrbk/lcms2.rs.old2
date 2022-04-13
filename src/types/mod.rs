37dbdd6771fc934eb52c40cb9fe03908af0c4515
Stefan Kewatt <stefan.kewatt@gmail.com>
2022-04-12 20:31:18 -0500

    Add CIEXYZ definition

--- src/types/mod.rs
+++ src/types/mod.rs
@@ -4,6 +4,7 @@ mod icc_header;
 mod profile;
 mod profile_id;
 mod signature;
+mod cie_xyz;
 
 pub use date_time_number::DateTimeNumber;
 pub use encoded_xyz_number::EncodedXYZNumber;
@@ -11,6 +12,7 @@ pub use icc_header::ICCHeader;
 pub use profile::Profile;
 pub use profile_id::ProfileID;
 pub use signature::Signature;
+pub use cie_xyz::CIEXYZ;
 
 pub mod signatures;
 
