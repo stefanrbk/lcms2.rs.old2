29279919b4da79e5365adb98e7c23b25bf2d7194
Stefan Kewatt <stefan.kewatt@gmail.com>
2022-04-12 20:30:17 -0500

    Finish FileNull

--- src/io/file_null.rs
+++ src/io/file_null.rs
@@ -1,11 +1,51 @@
-use std::io::{Read, Result};
+use std::io::{Read, Result, Write, Seek};
 
 pub struct FileNull {
     pointer: usize,
+    used_space: usize,
 }
 
 impl Read for FileNull {
     fn read(&mut self, buf: &mut [u8]) -> Result<usize> {
-        todo!()
+        let len = buf.len();
+        self.pointer += len;
+
+        Ok(len)
+    }
+}
+impl Write for FileNull {
+    fn write(&mut self, buf: &[u8]) -> Result<usize> {
+        let len = buf.len();
+        self.pointer += len;
+
+        if self.pointer > self.used_space {
+            self.used_space = self.pointer
+        }
+
+        Ok(len)
+    }
+
+    fn flush(&mut self) -> Result<()> {
+        Ok(())
+    }
+}
+impl Seek for FileNull {
+    fn seek(&mut self, pos: std::io::SeekFrom) -> Result<u64> {
+        match pos {
+            std::io::SeekFrom::Start(value) => {
+                self.pointer = value as usize;
+
+                Ok(value)
+            },
+            std::io::SeekFrom::End(value) => {
+                self.pointer = self.used_space + value as usize;
+
+                Ok(self.pointer as u64)
+            },
+            std::io::SeekFrom::Current(value) => {
+                self.pointer += value as usize;
+
+                Ok(self.pointer as u64)},
+        }
     }
 }
