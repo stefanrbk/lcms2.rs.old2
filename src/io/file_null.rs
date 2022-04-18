b32a9bfeb7f4b9ad99eef23b66a8d571083e59ff
Stefan Kewatt <stefan.kewatt@gmail.com>
2022-04-18 00:20:19 -0500

    Update FileNull with IOHandler as a trait

--- src/io/file_null.rs
+++ src/io/file_null.rs
@@ -1,6 +1,6 @@
-use std::io::{Read, Result, Write, Seek};
+use std::io::{Result, SeekFrom};
 
-use super::Stream;
+use super::IOHandler;
 
 pub struct FileNull {
     pub(crate) pointer: usize,
@@ -18,16 +18,39 @@ impl Default for FileNull {
         Self::new()
     }
 }
-impl Read for FileNull {
-    fn read(&mut self, buf: &mut [u8]) -> Result<usize> {
+
+impl IOHandler for FileNull {
+    fn read(&mut self, buf: &mut [u8]) -> Result<()> {
         let len = buf.len();
         self.pointer += len;
 
-        Ok(len)
+        Ok(())
     }
-}
-impl Write for FileNull {
-    fn write(&mut self, buf: &[u8]) -> Result<usize> {
+
+    fn seek(&mut self, pos: SeekFrom) -> Result<()> {
+        match pos {
+            SeekFrom::Start(value) => {
+                self.pointer = value as usize;
+            },
+            SeekFrom::End(value) => {
+                self.pointer = self.used_space + value as usize;
+            },
+            SeekFrom::Current(value) => {
+                self.pointer += value as usize;
+            }
+        }
+        Ok(())
+    }
+
+    fn close(&mut self) -> Result<()> {
+        Ok(())
+    }
+
+    fn tell(&mut self) -> usize {
+        self.pointer
+    }
+
+    fn write(&mut self, buf: &[u8]) -> Result<()> {
         let len = buf.len();
         self.pointer += len;
 
@@ -35,31 +58,6 @@ impl Write for FileNull {
             self.used_space = self.pointer
         }
 
-        Ok(len)
-    }
-
-    fn flush(&mut self) -> Result<()> {
         Ok(())
     }
 }
-impl Seek for FileNull {
-    fn seek(&mut self, pos: std::io::SeekFrom) -> Result<u64> {
-        match pos {
-            std::io::SeekFrom::Start(value) => {
-                self.pointer = value as usize;
-
-                Ok(value)
-            },
-            std::io::SeekFrom::End(value) => {
-                self.pointer = self.used_space + value as usize;
-
-                Ok(self.pointer as u64)
-            },
-            std::io::SeekFrom::Current(value) => {
-                self.pointer += value as usize;
-
-                Ok(self.pointer as u64)},
-        }
-    }
-}
-impl Stream for FileNull {}
