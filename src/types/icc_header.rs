aad5185cdbab9a93fa5a6be6587a321202f7e12d
Stefan Kewatt <stefan.kewatt@gmail.com>
2022-04-25 00:41:19 -0500

    Rename local variable with ICCHeaderConverter::swap_endianness

--- src/types/icc_header.rs
+++ src/types/icc_header.rs
@@ -52,55 +52,55 @@ pub(crate) union ICCHeaderConverter {
 }
 
 impl ICCHeaderConverter {
-    fn swap_endianness(header: ICCHeader) -> ICCHeader {
-        let mut value = header.clone();
+    fn swap_endianness(header: &ICCHeader) -> ICCHeader {
+        let mut header = header.clone();
 
-        value.size = adjust_endianness_u32(value.size);
-        value.cmm_id = Signature::from(adjust_endianness_u32(value.cmm_id.into()));
-        value.version = adjust_endianness_u32(value.version);
-        value.device_class = Signature::from(adjust_endianness_u32(value.device_class.into()));
-        value.color_space = Signature::from(adjust_endianness_u32(value.color_space.into()));
-        value.pcs = Signature::from(adjust_endianness_u32(value.pcs.into()));
-        value.date.year = adjust_endianness_u16(value.date.year);
-        value.date.month = adjust_endianness_u16(value.date.month);
-        value.date.day = adjust_endianness_u16(value.date.day);
-        value.date.hours = adjust_endianness_u16(value.date.hours);
-        value.date.minutes = adjust_endianness_u16(value.date.minutes);
-        value.date.seconds = adjust_endianness_u16(value.date.seconds);
-        value.magic = Signature::from(adjust_endianness_u32(value.magic.into()));
-        value.platform = Signature::from(adjust_endianness_u32(value.platform.into()));
-        value.flags = adjust_endianness_u32(value.flags);
-        value.manufacturer = Signature::from(adjust_endianness_u32(value.manufacturer.into()));
-        value.model = adjust_endianness_u32(value.model);
-        value.attributes = adjust_endianness_u64(value.attributes);
-        value.rendering_intent = adjust_endianness_u32(value.rendering_intent);
-        value.illuminant.x = unsafe {
+        header.size = adjust_endianness_u32(header.size);
+        header.cmm_id = Signature::from(adjust_endianness_u32(header.cmm_id.into()));
+        header.version = adjust_endianness_u32(header.version);
+        header.device_class = Signature::from(adjust_endianness_u32(header.device_class.into()));
+        header.color_space = Signature::from(adjust_endianness_u32(header.color_space.into()));
+        header.pcs = Signature::from(adjust_endianness_u32(header.pcs.into()));
+        header.date.year = adjust_endianness_u16(header.date.year);
+        header.date.month = adjust_endianness_u16(header.date.month);
+        header.date.day = adjust_endianness_u16(header.date.day);
+        header.date.hours = adjust_endianness_u16(header.date.hours);
+        header.date.minutes = adjust_endianness_u16(header.date.minutes);
+        header.date.seconds = adjust_endianness_u16(header.date.seconds);
+        header.magic = Signature::from(adjust_endianness_u32(header.magic.into()));
+        header.platform = Signature::from(adjust_endianness_u32(header.platform.into()));
+        header.flags = adjust_endianness_u32(header.flags);
+        header.manufacturer = Signature::from(adjust_endianness_u32(header.manufacturer.into()));
+        header.model = adjust_endianness_u32(header.model);
+        header.attributes = adjust_endianness_u64(header.attributes);
+        header.rendering_intent = adjust_endianness_u32(header.rendering_intent);
+        header.illuminant.x = unsafe {
             transmute::<u32, i32>(adjust_endianness_u32(transmute::<i32, u32>(
-                value.illuminant.x,
+                header.illuminant.x,
             )))
         };
-        value.illuminant.y = unsafe {
+        header.illuminant.y = unsafe {
             transmute::<u32, i32>(adjust_endianness_u32(transmute::<i32, u32>(
-                value.illuminant.y,
+                header.illuminant.y,
             )))
         };
-        value.illuminant.z = unsafe {
+        header.illuminant.z = unsafe {
             transmute::<u32, i32>(adjust_endianness_u32(transmute::<i32, u32>(
-                value.illuminant.z,
+                header.illuminant.z,
             )))
         };
-        value.creator = Signature::from(adjust_endianness_u32(value.creator.into()));
+        header.creator = Signature::from(adjust_endianness_u32(header.creator.into()));
 
-        value
+        header
     }
     pub fn from_bytes(bytes: [u8; 128]) -> ICCHeader {
         let converter = ICCHeaderConverter { bytes };
         unsafe {
-            Self::swap_endianness(converter.value)
+            Self::swap_endianness(&converter.value)
         }
     }
     pub fn to_bytes(value: ICCHeader) -> [u8; 128] {
-        let converter = ICCHeaderConverter { value: Self::swap_endianness(value) };
+        let converter = ICCHeaderConverter { value: Self::swap_endianness(&value) };
         unsafe {
             converter.bytes
         }
