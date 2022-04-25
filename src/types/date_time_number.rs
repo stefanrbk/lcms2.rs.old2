8433a76c89c1d561afe5b49d19ef88e5764ff931
Stefan Kewatt <stefan.kewatt@gmail.com>
2022-04-24 22:31:24 -0500

    Create casts between DateTimeNumber and chrono::NaiveDateTime

--- src/types/date_time_number.rs
+++ src/types/date_time_number.rs
@@ -1,3 +1,5 @@
+use chrono::{Datelike, Timelike};
+
 /// ICC date time
 #[derive(Copy, Clone, Debug, Eq, PartialEq)]
 #[repr(C)]
@@ -8,4 +10,29 @@ pub struct DateTimeNumber {
     pub hours: u16,
     pub minutes: u16,
     pub seconds: u16,
+}
+
+impl From<DateTimeNumber> for chrono::NaiveDateTime {
+    fn from(d: DateTimeNumber) -> Self {
+        chrono::NaiveDate::from_ymd(d.year as i32, d.month as u32, d.day as u32).and_hms(
+            d.hours as u32,
+            d.minutes as u32,
+            d.seconds as u32,
+        )
+    }
+}
+impl From<chrono::NaiveDateTime> for DateTimeNumber {
+    fn from(d: chrono::NaiveDateTime) -> Self {
+        let time = d.time();
+
+        let (_, year) = d.year_ce();
+        Self {
+            year: year as u16,
+            month: d.month() as u16,
+            day: d.day() as u16,
+            hours: time.hour() as u16,
+            minutes: time.minute() as u16,
+            seconds: time.second() as u16,
+        }
+    }
 }
