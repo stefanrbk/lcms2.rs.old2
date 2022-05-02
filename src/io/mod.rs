bd67eab1b59dd163b331d21eaf7954c4939d8f08
Stefan Kewatt <stefan.kewatt@gmail.com>
2022-05-01 19:05:38 -0500

    Update formatting

--- src/io/mod.rs
+++ src/io/mod.rs
@@ -1,8 +1,36 @@
+//! Traits, helpers, and type definitions for I/O functionality.
+//! 
+//! The `lcms2::io` module contains a number of common things you'll need when doing input and output with this library.
+//! The most core part of this module is the [`IOHandler`] trait, which standardizes the forms of I/O for the library.
+//! 
+//! # IOHandler
+//! 
+//! [`IOHandler`] is essentially a big wrapper for the [`Read`](std::io::Read), [`Write`](std::io::Write), and
+//! [`Seek`](std::io::Seek) traits, along with some helper functions.
+//! 
+//! # I/O Sources
+//! 
+//! This module contains 2 different types representing different I/O sources implementing the [`IOHandler`] trait and 1 
+//! retroactive implementation of an existing type. They are:
+//! - [`FileMem`]
+//! - [`FileNull`]
+//! - [`std::fs::File`]
+//! 
+//! # Endianness Helper Functions
+//! 
+//! Endianness is an important concept to understand and be aware of when handling any kind of I/O. The
+//! [`little_endian`] and [`big_endian`] modules assist in converting data from one format to another. Each module has
+//! the same functions, just different operations depending on which format you are intending on reading or writing into
+//! the format used by the CPU.
+//! 
+//! Since the main purpose of this library is for handling ICC profiles (which are supposed to be in big endian format),
+//! the [`big_endian`] module's functions are re-exported into this module to make it easier to distinguish which
+//! functions to use. This library does have an optional feature `use_little_endian` which swaps the re-exported
+//! functions to the [`little_endian`] module's versions if needed.
+
 mod io_handler;
-#[cfg_attr(tarpaulin, skip)]
-mod little_endian;
-#[cfg_attr(tarpaulin, skip)]
-mod big_endian;
+pub mod little_endian;
+pub mod big_endian;
 mod file_null;
 mod file_mem;
 mod file_file;
--- src/lib.rs
+++ src/lib.rs
@@ -6,19 +6,19 @@
 //!
 //! Taking from the original README file:
 //! # About Little CMS
-//! 
+//!
 //! Little CMS intends to be an OPEN SOURCE small-footprint color management engine, with special focus on accuracy and
 //! performance. It uses the International Color Consortium standard (ICC), which is the modern standard when regarding
 //! to color management. The ICC specification is widely used and is referred to in many International and other
 //! de-facto standards. It was approved as an International Standard, ISO 15076-1, in 2005.
 //!
 //! # Conformance
-//! 
+//!
 //! Little CMS is a FULL IMPLEMENTATION of ICC specification 4.3, it fully supports all kind of V2 and V4 profiles,
 //! including abstract, devicelink and named color profiles. Check the tutorial for a exhaustive list of features.
 //!
 //! # A bit of story
-//! 
+//!
 //! Since the initial release, back in 1998, Little CMS has grown to become one of the most popular open-source color
 //! management libraries, and has been used in a large number of production projects, in areas as printer firmware,
 //! monitors, digital cameras, RIPs, publishing, scientific, and many others. You can find Little CMS in most Linux
@@ -34,11 +34,11 @@ macro_rules! use_big_endian {
 
 // Fixed Point Types
 
-/// Fixed point [`UQ8.8`](https://en.wikipedia.org/wiki/Q_(number_format)) Number
+/// Fixed point [UQ8.8](https://en.wikipedia.org/wiki/Q_(number_format)) Number
 pub type U8F8 = u16;
-/// Fixed point [`Q15.16`](https://en.wikipedia.org/wiki/Q_(number_format)) Number
+/// Fixed point [Q15.16](https://en.wikipedia.org/wiki/Q_(number_format)) Number
 pub type S15F16 = i32;
-/// Fixed point [`UQ16.16`](https://en.wikipedia.org/wiki/Q_(number_format)) Number
+/// Fixed point [UQ16.16](https://en.wikipedia.org/wiki/Q_(number_format)) Number
 pub type U16F16 = u32;
 
 pub mod io;
