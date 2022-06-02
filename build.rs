7374d2c8760e57c2ffef5a88b0d4699e98642b3c
Stefan Kewatt <stefan.kewatt@gmail.com>
2022-06-02 04:56:51 -0500

    Make build script only run on dev builds

--- build.rs
+++ build.rs
@@ -1,5 +1,7 @@
 fn main() {
-    // Tell cargo to tell rustc to link the lcms2 library from tests/include.
-    println!("cargo:rustc-link-search=native=./tests/include");
-    println!("cargo:rustc-link-lib=static=lcms2");
+    if cfg!(dev) {
+        // Tell cargo to tell rustc to link the lcms2 library from tests/include.
+        println!("cargo:rustc-link-search=native=./tests/include");
+        println!("cargo:rustc-link-lib=static=lcms2");
+    }
 }
