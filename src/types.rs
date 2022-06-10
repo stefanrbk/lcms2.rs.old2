db812b3c943deb0720e536a2c79f23b068927334
Stefan Kewatt <stefan.kewatt@gmail.com>
2022-06-10 08:03:42 -0500

    Add Pipeline types

--- src/types.rs
+++ src/types.rs
@@ -6,6 +6,7 @@ mod profile_id;
 mod signature;
 mod cie_xyz;
 mod tag_entry;
+mod pipeline;
 
 pub use date_time_number::DateTimeNumber;
 pub use encoded_xyz_number::EncodedXYZNumber;
@@ -16,8 +17,14 @@ pub use signature::Signature;
 pub use cie_xyz::CIEXYZ;
 pub use tag_entry::TagEntry;
 
+pub use pipeline::Pipeline;
+pub use pipeline::PipelineEvalFn;
+pub use pipeline::Stage;
+pub use pipeline::StageEvalFn;
+
 
 #[allow(missing_docs)]
 pub mod signatures;
 
 pub const MAX_TABLE_TAG: usize = 100;
+pub const MAX_CHANNELS: usize = 16;
---
+++ src/types/pipeline.rs
@@ -0,0 +1,30 @@
+use std::sync::{Arc, Mutex};
+
+use crate::state::Context;
+
+use super::Signature;
+
+pub type StageEvalFn = fn(r#in: &[f32], out: &mut [f32], mpe: &Stage);
+pub struct Stage {
+    context: Arc<Mutex<Context>>,
+    r#type: Signature,
+    implements: Signature,
+    input_channels: u32,
+    output_channels: u32,
+    eval: StageEvalFn,
+    data: Arc<Box<[u8]>>,
+}
+
+pub enum PipelineEvalFn {
+    U16(fn(r#in: &[u16], out: &mut [u16], data: Box<[u8]>)),
+    Float(fn(r#in: &[f32], out: &mut [f32], data: Box<[u8]>))
+}
+pub struct Pipeline {
+    elements: Vec<Arc<Stage>>,
+    input_channels: u32,
+    output_channels: u32,
+    data: Arc<Box<[u8]>>,
+    eval: PipelineEvalFn,
+    context: Arc<Mutex<Context>>,
+    save_as_8_bits: bool,
+}
