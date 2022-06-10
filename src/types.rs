cff750137bb38492b5ec38e2e1a6d01f55ca4c60
Stefan Kewatt <stefan.kewatt@gmail.com>
2022-06-10 09:47:07 -0500

    Add CurveSegment types

--- src/types.rs
+++ src/types.rs
@@ -1,4 +1,5 @@
 mod cie_xyz;
+mod curve_segment;
 mod date_time_number;
 mod encoded_xyz_number;
 mod icc_header;
@@ -13,6 +14,7 @@ mod tag_entry;
 mod transform;
 
 pub use cie_xyz::CIEXYZ;
+pub use curve_segment::CurveSegment;
 pub use date_time_number::DateTimeNumber;
 pub use encoded_xyz_number::EncodedXYZNumber;
 pub use icc_header::ICCHeader;
---
+++ src/types/curve_segment.rs
@@ -0,0 +1,8 @@
+pub struct CurveSegment {
+    x0: f32,
+    x1: f32,
+    r#type: i32,
+    params: [f64; 10],
+    num_grid_points: usize,
+    sampled_points: [f32]
+}
