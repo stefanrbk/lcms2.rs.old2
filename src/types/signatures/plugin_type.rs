a511228fd86f6c9a52c44447c8a3722e23765d08
Stefan Kewatt <stefan.kewatt@gmail.com>
2022-06-21 13:18:21 -0500

    Add plugin type signatures

--- src/types/signatures/plugin_type.rs
+++ src/types/signatures/plugin_type.rs
@@ -2,3 +2,11 @@ use super::Signature;
 
 pub const MAGIC: Signature = Signature::new(b"acpp");
 pub const TRANSFORM: Signature = Signature::new(b"xfmH");
+pub const INTERPOLATION: Signature = Signature::new(b"inpH");
+pub const TAG_TYPE: Signature = Signature::new(b"typH");
+pub const TAG: Signature = Signature::new(b"tagH");
+pub const FORMATTERS: Signature = Signature::new(b"frmH");
+pub const RENDERING_INTENT: Signature = Signature::new(b"intH");
+pub const PARAMETRIC_CURVE: Signature = Signature::new(b"parH");
+pub const MULTI_PROCESS_ELEMENT: Signature = Signature::new(b"mpeH");
+pub const OPTIMIZATION: Signature = Signature::new(b"optH");
