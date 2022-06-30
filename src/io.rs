058025ea0557772a75c2d322bd5f264d5cab91c8
Stefan Kewatt <stefan.kewatt@gmail.com>
2022-06-29 20:34:10 -0500

    Update endian mods as little endian is not needed

--- src/io.rs
+++ src/io.rs
@@ -18,19 +18,10 @@
 //! 
 //! # Endianness Helper Functions
 //! 
-//! Endianness is an important concept to understand and be aware of when handling any kind of I/O. The
-//! [`little_endian`] and [`big_endian`] modules assist in converting data from one format to another. Each module has
-//! the same functions, just different operations depending on which format you are intending on reading or writing into
-//! the format used by the CPU.
-//! 
-//! Since the main purpose of this library is for handling ICC profiles (which are supposed to be in big endian format),
-//! the [`big_endian`] module's functions are re-exported into this module to make it easier to distinguish which
-//! functions to use. This library does have an optional feature `use_little_endian` which swaps the re-exported
-//! functions to the [`little_endian`] module's versions if needed.
+//! ICC profiles are to be in big endian format. Since most CPUs are little endian these days, conversion functions are
+//! included to handle the conversions.
 
 mod io_handler;
-pub mod little_endian;
-pub mod big_endian;
 mod file_null;
 mod file_mem;
 mod file_file;
@@ -41,32 +32,44 @@ pub use io_handler::IOHandler;
 pub use file_null::FileNull;
 pub use file_mem::FileMem;
 
-#[cfg(feature = "use_little_endian")]
-pub use little_endian::adjust_endianness_16;
-#[cfg(feature = "use_little_endian")]
-pub use little_endian::adjust_endianness_32;
-#[cfg(feature = "use_little_endian")]
-pub use little_endian::adjust_endianness_64;
-#[cfg(feature = "use_little_endian")]
-pub use little_endian::adjust_endianness_u16;
-#[cfg(feature = "use_little_endian")]
-pub use little_endian::adjust_endianness_u32;
-#[cfg(feature = "use_little_endian")]
-pub use little_endian::adjust_endianness_u64;
+/// Convert a `[u8; 2]` array to/from big endian.
+#[inline(always)]
+pub fn adjust_endianness_16(value: [u8; 2]) -> [u8; 2] {
+    let value = u16::from_be_bytes(value);
+    value.to_ne_bytes()
+}
 
+/// Convert a `[u8; 4]` array to/from big endian.
+#[inline(always)]
+pub fn adjust_endianness_32(value: [u8; 4]) -> [u8; 4] {
+    let value = u32::from_be_bytes(value);
+    value.to_ne_bytes()
+}
 
-#[cfg(not(feature = "use_little_endian"))]
-pub use big_endian::adjust_endianness_16;
-#[cfg(not(feature = "use_little_endian"))]
-pub use big_endian::adjust_endianness_32;
-#[cfg(not(feature = "use_little_endian"))]
-pub use big_endian::adjust_endianness_64;
-#[cfg(not(feature = "use_little_endian"))]
-pub use big_endian::adjust_endianness_u16;
-#[cfg(not(feature = "use_little_endian"))]
-pub use big_endian::adjust_endianness_u32;
-#[cfg(not(feature = "use_little_endian"))]
-pub use big_endian::adjust_endianness_u64;
+/// Convert a `[u8; 8]` array to/from big endian.
+#[inline(always)]
+pub fn adjust_endianness_64(value: [u8; 8]) -> [u8; 8] {
+    let value = u64::from_be_bytes(value);
+    value.to_ne_bytes()
+}
+
+/// Convert a [`u16`] to/from big endian.
+#[inline(always)]
+pub fn adjust_endianness_u16(value: u16) -> u16 {
+    value.to_be()
+}
+
+/// Convert a [`u32`] to/from big endian.
+#[inline(always)]
+pub fn adjust_endianness_u32(value: u32) -> u32 {
+    value.to_be()
+}
+
+/// Convert a [`u64`] to/from big endian.
+#[inline(always)]
+pub fn adjust_endianness_u64(value: u64) -> u64 {
+    value.to_be()
+}
 
 /// Enumeration of possible I/O actions
 pub enum AccessMode {
