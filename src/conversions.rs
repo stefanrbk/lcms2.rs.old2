4cc29345f795ba6d11a9403473f53e83e48bcd1a
Stefan Kewatt <stefan.kewatt@gmail.com>
2022-06-29 19:51:14 -0500

    Add whitepoint mod

--- src/conversions.rs
+++ src/conversions.rs
@@ -1,3 +1,4 @@
 #![allow(non_snake_case)]
 
 pub(crate) mod pcs;
+pub(crate) mod whitepoint;
---
+++ src/conversions/whitepoint.rs
@@ -0,0 +1,3 @@
+use crate::types::CIEXYZ;
+
+pub const D50_XYZ: CIEXYZ = CIEXYZ { X: 0.9642, Y: 1.0, Z: 0.8249 };
