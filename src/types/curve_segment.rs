8f3598dcf30585b44432ce4fb074915d538bc5f0
Stefan Kewatt <stefan.kewatt@gmail.com>
2022-07-05 20:34:33 -0500

    Expose fields of CurveSegment to the crate

--- src/types/curve_segment.rs
+++ src/types/curve_segment.rs
@@ -1,7 +1,7 @@
 pub struct CurveSegment {
-    x0: f32,
-    x1: f32,
-    r#type: i32,
-    params: [f64; 10],
-    sampled_points: Vec<f32>,
+    pub(crate) x0: f32,
+    pub(crate) x1: f32,
+    pub(crate) r#type: i32,
+    pub(crate) params: [f64; 10],
+    pub(crate) sampled_points: Vec<f32>,
 }
