f2595b9b95cf8c93d9d42b3b323f558fb2bdcba4
Stefan Kewatt <stefan.kewatt@gmail.com>
2022-07-06 00:44:07 -0500

    Add more fixed point conversions

--- src/math.rs
+++ src/math.rs
@@ -1,4 +1,4 @@
-use crate::U8F8;
+use crate::{U8F8, S15F16};
 
 #[inline]
 pub fn u8f8_to_f64(fixed8: U8F8) -> f64 {
@@ -6,4 +6,29 @@ pub fn u8f8_to_f64(fixed8: U8F8) -> f64 {
     let msb = ((fixed8 >> 8) & 0xFF) as u8;
     
     msb as f64 + (lsb as f64 / 256.0)
+}
+
+#[inline]
+pub fn f64_to_u8f8(val: f64) -> U8F8 {
+    let gamma_fixed_32 = f64_to_s15f16(val);
+    ((gamma_fixed_32 >> 8) & 0xFFFF) as U8F8
+}
+
+#[inline]
+pub fn s15f16_to_f64(fix32: S15F16) -> f64 {
+    let sign = if fix32 < 0 { -1.0 } else { 1.0 };
+    let fix32 = fix32.abs();
+
+    let whole = ((fix32 >> 16) & 0xFFFF) as u16;
+    let frac = (fix32 & 0xFFFF) as u16;
+
+    let mid = frac as f64 / 65536.0;
+    let floater = whole as f64 + mid;
+    
+    sign * floater
+}
+
+#[inline]
+pub fn f64_to_s15f16(val: f64) -> S15F16 {
+    (val * 65536.0 + 0.5) as S15F16
 }
