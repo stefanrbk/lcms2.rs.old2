4ee1932aa309b01810533bf7bee1522f0c21b272
Stefan Kewatt <stefan.kewatt@gmail.com>
2022-07-14 17:12:21 -0500

    Move chromaticity read/write functions into new mod

--- src/plugins/tag_type.rs
+++ src/plugins/tag_type.rs
@@ -57,42 +57,6 @@ impl Debug for TypeHandler {
 }
 
 impl TypeHandler {
-    pub fn chromaticity_read(
-        &self,
-        _context: &mut Context,
-        io: &mut dyn IOHandler,
-        size_of_tag: usize,
-    ) -> Result<(usize, Box<dyn Any>)> {
-        let mut chans = io.read_u16()?;
-
-        // Let's recover from a bug introduced in early versions of the original lcms1
-        if chans == 0 && size_of_tag == 32 {
-            _ = io.read_u16();
-            chans = io.read_u16()?;
-        }
-
-        if chans != 3 {
-            return Err(ErrorKind::InvalidData.into());
-        }
-        _ = io.read_u16()?;
-        let red = CIExyY {
-            x: io.read_s15f16()?,
-            y: io.read_s15f16()?,
-            Y: 1.0,
-        };
-        let green = CIExyY {
-            x: io.read_s15f16()?,
-            y: io.read_s15f16()?,
-            Y: 1.0,
-        };
-        let blue = CIExyY {
-            x: io.read_s15f16()?,
-            y: io.read_s15f16()?,
-            Y: 1.0,
-        };
-
-        Ok((1, Box::new(CIExyYTriple { red, green, blue })))
-    }
     pub fn colorant_order_read(
         &self,
         _context: &mut Context,
@@ -418,23 +382,7 @@ impl TypeHandler {
         Ok((1, Box::new(io.read_xyz()?)))
     }
 
-    /// ptr MUST be a &Box<CIExyYTriple>
-    pub fn chromaticity_write(
-        &self,
-        _context: &mut Context,
-        io: &mut dyn IOHandler,
-        ptr: Box<dyn Any>,
-        _num_items: usize,
-    ) -> Result<()> {
-        let value = ptr.downcast::<CIExyYTriple>().unwrap();
 
-        io.write_u16(3)?;
-        io.write_u16(0)?;
-
-        save_one_chromaticity(io, value.red.x, value.red.y)?;
-        save_one_chromaticity(io, value.green.x, value.green.y)?;
-        save_one_chromaticity(io, value.blue.x, value.blue.y)
-    }
     /// ptr MUST be &Box<Vec<u8>>
     pub fn colorant_order_write(
         &self,
@@ -842,11 +790,6 @@ impl TypeHandler {
     }
 }
 
-fn save_one_chromaticity(io: &mut dyn IOHandler, x: f64, y: f64) -> Result<()> {
-    io.write_s15f16(x)?;
-    io.write_s15f16(y)
-}
-
 pub fn read_16bit_tables(
     context: &mut Context,
     io: &mut dyn IOHandler,
@@ -1049,3 +992,5 @@ fn write_8bit_tables(
 
     Ok(())
 }
+
+pub(crate) mod chromaticity_tag;
---
+++ src/plugins/tag_type/chromaticity_tag.rs
@@ -0,0 +1,66 @@
+use std::{
+    any::Any,
+    io::{ErrorKind, Result},
+};
+
+use crate::{
+    io::IOHandler,
+    state::Context,
+    types::{CIExyY, CIExyYTriple},
+};
+
+pub fn read(
+    _context: &mut Context,
+    io: &mut dyn IOHandler,
+    size_of_tag: usize,
+) -> Result<(usize, Box<dyn Any>)> {
+    let mut chans = io.read_u16()?;
+
+    // Let's recover from a bug introduced in early versions of the original lcms1
+    if chans == 0 && size_of_tag == 32 {
+        _ = io.read_u16();
+        chans = io.read_u16()?;
+    }
+
+    if chans != 3 {
+        return Err(ErrorKind::InvalidData.into());
+    }
+    _ = io.read_u16()?;
+    let red = CIExyY {
+        x: io.read_s15f16()?,
+        y: io.read_s15f16()?,
+        Y: 1.0,
+    };
+    let green = CIExyY {
+        x: io.read_s15f16()?,
+        y: io.read_s15f16()?,
+        Y: 1.0,
+    };
+    let blue = CIExyY {
+        x: io.read_s15f16()?,
+        y: io.read_s15f16()?,
+        Y: 1.0,
+    };
+
+    Ok((1, Box::new(CIExyYTriple { red, green, blue })))
+}
+/// ptr MUST be a Box<&CIExyYTriple>
+pub fn write(
+    _context: &mut Context,
+    io: &mut dyn IOHandler,
+    ptr: Box<dyn Any>,
+    _num_items: usize,
+) -> Result<()> {
+    let value = ptr.downcast::<&CIExyYTriple>().unwrap();
+
+    io.write_u16(3)?;
+    io.write_u16(0)?;
+
+    save_one_chromaticity(io, value.red.x, value.red.y)?;
+    save_one_chromaticity(io, value.green.x, value.green.y)?;
+    save_one_chromaticity(io, value.blue.x, value.blue.y)
+}
+fn save_one_chromaticity(io: &mut dyn IOHandler, x: f64, y: f64) -> Result<()> {
+    io.write_s15f16(x)?;
+    io.write_s15f16(y)
+}
